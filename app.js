22cca6670b3d382cf2318c91dfc96e1f87a11fc5
Jibros <jibro718@gmail.com>
2022-05-10 18:59:37 +0900

    1차 샘플 - 근데 이거 아니라고 함
    해당 영역을 클릭해서 픽커컬러를 설정하고, 해당 영역의 질감을 선택해서 입힐 수 있어야함.

--- app.js
+++ app.js
@@ -1,66 +1,160 @@
-const fishHead = document.getElementById('fishHead');
-const fishBody1 = document.getElementById('fishBody1');
-const fishBody2 = document.getElementById('fishBody2');
-const fishBody3 = document.getElementById('fishBody3');
-const fishBody4 = document.getElementById('fishBody4');
-const fishBody5 = document.getElementById('fishBody5');
-const fishTop = document.getElementById('fishTop');
-const fishBottom = document.getElementById('fishBottom');
-const fishTail = document.getElementById('fishTail');
-fishHead.addEventListener('click', () => {    
-    let fishHead = document.getElementById("colorFishHead")
-    fishHead.addEventListener("click", () => {
-        
-    })
-    changeColor(fishHead);
+// https://seballot.github.io/spectrum/#toc0
+
+const fishHead = $('#svgFishHead');
+const fishBody1 = $('#svgFishBody1');
+const fishBody2 = $('#svgFishBody2');
+const fishBody3 = $('#svgFishBody3');
+const fishBody4 = $('#svgFishBody4');
+const fishBody5 = $('#svgFishBody5');
+const fishTop = $('#svgFishTop');
+const fishBottom = $('#svgFishBottom');
+const fishTail = $('#svgFishTail');
+fishHead.on('click', () => {    
+    
 })
-fishBody1.addEventListener('click', () => {
+fishBody1.on('click', () => {
     alert("fishBody1")
 })
-fishBody2.addEventListener('click', () => {
+fishBody2.on('click', () => {
     alert("fishBody2")
 })
-fishBody3.addEventListener('click', () => {
+fishBody3.on('click', () => {
     alert("fishBody3")
 })
-fishBody4.addEventListener('click', () => {
+fishBody4.on('click', () => {
     alert("fishBody4")
 })
-fishBody5.addEventListener('click', () => {
+fishBody5.on('click', () => {
     alert("fishBody5")
 })
-fishTop.addEventListener('click', () => {
+fishTop.on('click', () => {
     alert("fishTop")
 })
-fishBottom.addEventListener('click', () => {
+fishBottom.on('click', () => {
     alert("fishBottom")
 })
-fishTail.addEventListener('click', () => {
+fishTail.on('click', () => {
     alert("fishTail")
 })
 
-var colorWell;
+$("#colorFishHead").spectrum({
+    showPalette: false,
+    showButtons: false,
+    showAlpha: true,
+    move: function(color){
+        colorChange(fishHead, color)
+    }
+});
+$("#colorFishBody1").spectrum({
+    showPalette: false,
+    showButtons: false,
+    move: function(color){
+        colorChange(fishBody1, color)
+    }
+});
+$("#colorFishBody2").spectrum({
+    showPalette: false,
+    showButtons: false,
+    move: function(color){
+        colorChange(fishBody2, color)
+    }
+});
+$("#colorFishBody3").spectrum({
+    showPalette: false,
+    showButtons: false,
+    move: function(color){
+        colorChange(fishBody3, color)
+    }
+});
+$("#colorFishBody4").spectrum({
+    showPalette: false,
+    showButtons: false,
+    move: function(color){
+        colorChange(fishBody4, color)
+    }
+});
+$("#colorFishBody5").spectrum({
+    showPalette: false,
+    showButtons: false,
+    move: function(color){
+        colorChange(fishBody5, color)
+    }
+});
+$("#colorFishTop").spectrum({
+    showPalette: false,
+    showButtons: false,
+    move: function(color){
+        colorChange(fishTop, color)
+    }
+});
+$("#colorFishBottom").spectrum({
+    showPalette: false,
+    showButtons: false,
+    move: function(color){
+        colorChange(fishBottom, color)
+    }
+});
+$("#colorFishTail").spectrum({
+    showPalette: false,
+    //showButtons: false,
+    change: function(color){
+        colorChange(fishTail, color)
+    }
+});
 
-function changeColor(obj) {
-    colorWell = obj;
-    console.log(colorWell)
-    colorWell.addEventListener("input", updateFirst, false);
-    colorWell.addEventListener("change", updateAll, false);
-    //colorWell.select();
+const colorChange = function(obj, color) {
+    let _color = color.toRgb();
+    let haxcode = "rgb("+_color.r+","+_color.g+","+_color.b+")";
+    let _obj = obj;
+    console.log(haxcode)
+    if(_color.a < 1){
+        $(_obj).css({"fill": rgb2hex(haxcode), "fill-opacity": _color.a})
+    }else{
+        $(_obj).css({"fill": rgb2hex(haxcode)})
+    }
+    
+
+    
 }
 
-// 선택하는 동안 색 변경
-function updateFirst(event) {
-    var p = document.querySelector("p");
-    console.log("ss")
-    if (p) {
-      p.style.color = event.target.value;
-    }
-  }
 
-  // 선택이 끝난 후 닫혔을 때 색 변경
-  function updateAll(event) {
-    document.querySelectorAll("p").forEach(function(p) {
-      p.style.color = event.target.value;
-    });
-  }
+
+function defatulColorSet(){
+    // 물고기 색 피커 세팅
+    const defaultHeadColor = fishHead.css("fill");
+    const defaultBody1Color = fishBody1.css("fill");
+    const defaultBody2Color = fishBody2.css("fill");
+    const defaultBody3Color = fishBody3.css("fill");
+    const defaultBody4Color = fishBody4.css("fill");
+    const defaultBody5Color = fishBody5.css("fill");
+    const defaultTopColor = fishTop.css("fill");
+    const defaultBottomColor = fishBottom.css("fill");
+    const defaultTailColor = fishTail.css("fill");
+    
+    $("#colorFishHead").spectrum("set", rgb2hex(defaultHeadColor));
+    $("#colorFishBody1").spectrum("set", rgb2hex(defaultBody1Color));
+    $("#colorFishBody2").spectrum("set", rgb2hex(defaultBody2Color));
+    $("#colorFishBody3").spectrum("set", rgb2hex(defaultBody3Color));
+    $("#colorFishBody4").spectrum("set", rgb2hex(defaultBody4Color));
+    $("#colorFishBody5").spectrum("set", rgb2hex(defaultBody5Color));
+    $("#colorFishTop").spectrum("set", rgb2hex(defaultTopColor));
+    $("#colorFishBottom").spectrum("set", rgb2hex(defaultBottomColor));
+    $("#colorFishTail").spectrum("set", rgb2hex(defaultTailColor));
+}
+// rgb to hex 
+function rgb2hex(rgb) {
+    if (  rgb.search("rgb") == -1 ) {
+         return rgb;
+    } else {
+         rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
+         function hex(x) {
+              return ("0" + parseInt(x).toString(16)).slice(-2);
+         }
+         return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]); 
+    }
+}
+
+$(function(){
+    //Init 
+    defatulColorSet()
+})
