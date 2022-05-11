135e81e384cf34edf7908d7e653c123a9eba9ae0
Jibros <jibro718@gmail.com>
2022-05-12 00:49:26 +0900

    1차 샘플 - 완료 후 커밋

--- app.js
+++ app.js
@@ -9,116 +9,139 @@ const fishBody5 = $('#svgFishBody5');
 const fishTop = $('#svgFishTop');
 const fishBottom = $('#svgFishBottom');
 const fishTail = $('#svgFishTail');
-fishHead.on('click', () => {    
-    
-})
-fishBody1.on('click', () => {
-    alert("fishBody1")
-})
-fishBody2.on('click', () => {
-    alert("fishBody2")
-})
-fishBody3.on('click', () => {
-    alert("fishBody3")
-})
-fishBody4.on('click', () => {
-    alert("fishBody4")
-})
-fishBody5.on('click', () => {
-    alert("fishBody5")
-})
-fishTop.on('click', () => {
-    alert("fishTop")
-})
-fishBottom.on('click', () => {
-    alert("fishBottom")
-})
-fishTail.on('click', () => {
-    alert("fishTail")
-})
 
-$("#colorFishHead").spectrum({
-    showPalette: false,
-    showButtons: false,
-    showAlpha: true,
-    move: function(color){
-        colorChange(fishHead, color)
+
+// rgb to hex 
+const rgb2hex = function(rgb) {
+    if (  rgb.search("rgb") == -1 ) {
+         return rgb;
+    } else {
+         rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
+         function hex(x) {
+              return ("0" + parseInt(x).toString(16)).slice(-2);
+         }
+         return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]); 
     }
-});
-$("#colorFishBody1").spectrum({
-    showPalette: false,
-    showButtons: false,
-    move: function(color){
-        colorChange(fishBody1, color)
-    }
-});
-$("#colorFishBody2").spectrum({
-    showPalette: false,
-    showButtons: false,
-    move: function(color){
-        colorChange(fishBody2, color)
-    }
-});
-$("#colorFishBody3").spectrum({
-    showPalette: false,
-    showButtons: false,
-    move: function(color){
-        colorChange(fishBody3, color)
-    }
-});
-$("#colorFishBody4").spectrum({
-    showPalette: false,
-    showButtons: false,
-    move: function(color){
-        colorChange(fishBody4, color)
-    }
-});
-$("#colorFishBody5").spectrum({
-    showPalette: false,
-    showButtons: false,
-    move: function(color){
-        colorChange(fishBody5, color)
-    }
-});
-$("#colorFishTop").spectrum({
-    showPalette: false,
-    showButtons: false,
-    move: function(color){
-        colorChange(fishTop, color)
-    }
-});
-$("#colorFishBottom").spectrum({
-    showPalette: false,
-    showButtons: false,
-    move: function(color){
-        colorChange(fishBottom, color)
-    }
-});
-$("#colorFishTail").spectrum({
+}
+/* 
+const colorChange = function(obj, color) {
+    let _color = color.toRgb();
+    console.log(_color)
+    let haxcode = "rgb("+_color.r+","+_color.g+","+_color.b+")";
+    let _obj = obj;
+    
+    console.log(haxcode, "haxcode  sdfsdfad")
+    
+    $(_obj).css({"fill": haxcode, "fill-opacity": _color.a})
+
+} */
+
+const config = {
+    flat:false,
+    showAlpha: false,
     showPalette: false,
     //showButtons: false,
     change: function(color){
-        colorChange(fishTail, color)
+        console.log("color " ,color)
+        //colorChange(this, color)
+        if(color != null){
+            $(this).css({"fill": color, "fill-opacity": color.a})
+        }
+    }
+}
+fishHead.spectrum(config);
+fishBody1.spectrum(config);
+fishBody2.spectrum(config);
+fishBody3.spectrum(config);
+fishBody4.spectrum(config);
+fishBody5.spectrum(config);
+fishTop.spectrum(config);
+fishBottom.spectrum(config);
+fishTail.spectrum(config);
+
+/* 
+
+$(function(){
+    $("#colorFishHead").spectrum({
+        showPalette: false,
+        showButtons: false,
+        showAlpha: true,
+        move: function(color){
+            colorChange(fishHead, color)
+        }
+    });
+    $("#colorFishBody1").spectrum({
+        showPalette: false,
+        showButtons: false,
+        move: function(color){
+            colorChange(fishBody1, color)
+        }
+    });
+    $("#colorFishBody2").spectrum({
+        showPalette: false,
+        showButtons: false,
+        move: function(color){
+            colorChange(fishBody2, color)
+        }
+    });
+    $("#colorFishBody3").spectrum({
+        showPalette: false,
+        showButtons: false,
+        move: function(color){
+            colorChange(fishBody3, color)
+        }
+    });
+    $("#colorFishBody4").spectrum({
+        showPalette: false,
+        showButtons: false,
+        move: function(color){
+            colorChange(fishBody4, color)
+        }
+    });
+    $("#colorFishBody5").spectrum({
+        showPalette: false,
+        showButtons: false,
+        move: function(color){
+            colorChange(fishBody5, color)
+        }
+    });
+    $("#colorFishTop").spectrum({
+        showPalette: false,
+        showButtons: false,
+        move: function(color){
+            colorChange(fishTop, color)
+        }
+    });
+    $("#colorFishBottom").spectrum({
+        showPalette: false,
+        showButtons: false,
+        move: function(color){
+            colorChange(fishBottom, color)
+        }
+    });
+    $("#colorFishTail").spectrum({
+        showPalette: false,
+        //showButtons: false,
+        change: function(color){
+            colorChange(fishTail, color)
+        }
+    });
+
+    const colorChange = function(obj, color) {
+        let _color = color.toRgb();
+        let haxcode = "rgb("+_color.r+","+_color.g+","+_color.b+")";
+        let _obj = obj;
+        console.log(haxcode)
+        if(_color.a < 1){
+            $(_obj).css({"fill": rgb2hex(haxcode), "fill-opacity": _color.a})
+        }else{
+            $(_obj).css({"fill": rgb2hex(haxcode)})
+        }
+        
+
+        
     }
 });
-
-const colorChange = function(obj, color) {
-    let _color = color.toRgb();
-    let haxcode = "rgb("+_color.r+","+_color.g+","+_color.b+")";
-    let _obj = obj;
-    console.log(haxcode)
-    if(_color.a < 1){
-        $(_obj).css({"fill": rgb2hex(haxcode), "fill-opacity": _color.a})
-    }else{
-        $(_obj).css({"fill": rgb2hex(haxcode)})
-    }
-    
-
-    
-}
-
-
-
 function defatulColorSet(){
     // 물고기 색 피커 세팅
     const defaultHeadColor = fishHead.css("fill");
@@ -157,4 +180,4 @@ function rgb2hex(rgb) {
 $(function(){
     //Init 
     defatulColorSet()
-})
+}) */
